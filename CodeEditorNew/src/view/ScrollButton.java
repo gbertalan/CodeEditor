259f893ea3daeb0dd5c2b017c85e345665e01580
Gergely Bertalan <bertalanofficial@gmail.com>
2024-07-05 11:19:50 +0200

    Make ScrollPanel actually scrollable

--- CodeEditorNew/src/view/ScrollButton.java
+++ CodeEditorNew/src/view/ScrollButton.java
@@ -10,30 +10,35 @@ import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import utils.Globals;
 import utils.Theme;
 
 public class ScrollButton extends JPanel implements MouseListener, MouseMotionListener, MouseWheelListener {
 
+	private ScrollPanel parent;
 	private int width;
 	private static int HEIGHT = 30;
 	private static final int TEXT_LEFT_MARGIN = 30;
-	
+
 	private String text;
 	private static int buttonCounter = 0;
 
 	private boolean entered;
-	
-	private int ID;
 
-	public ScrollButton(String text, int width) {
+	private int ID;
+	private static int scrolledBy;
+
+	public ScrollButton(String text, ScrollPanel parent) {
 
 		this.text = text;
-		this.width = width; 
+		this.parent = parent;
+		this.width = parent.getWidth();
 
 		setLayout(null);
 		setBounds(0, buttonCounter * (HEIGHT - 2), width, HEIGHT);
+//		setLocation(getX(), getY()+scrolledBy);
 
 		addMouseListener(this);
 		addMouseMotionListener(this);
@@ -42,9 +47,16 @@ public class ScrollButton extends JPanel implements MouseListener, MouseMotionLi
 		++buttonCounter;
 		this.ID = buttonCounter;
 	}
-	
+
 	public static void resetCounter() {
 		buttonCounter = 0;
+	}
+
+	public void scroll(int amount) {
+		System.out.println("amount: " + amount);
+		scrolledBy = getY() + amount;
+		setLocation(getX(), scrolledBy);
+
 	}
 
 	@Override
@@ -53,16 +65,14 @@ public class ScrollButton extends JPanel implements MouseListener, MouseMotionLi
 
 		Graphics2D g2d = (Graphics2D) g;
 		Globals.setRenderingHints(g2d);
-		
-		getParent().repaint();
 
 		if (entered)
 			g2d.setColor(Theme.getPanelButtonHoverColor());
 		else
 			g2d.setColor(getParent().getBackground());
-		
+
 		g2d.fillRect(0, 0, width, HEIGHT);
-		
+
 		g2d.setColor(Theme.getPanelTextColor());
 		g2d.drawString(text, TEXT_LEFT_MARGIN, 20);
 
@@ -71,7 +81,7 @@ public class ScrollButton extends JPanel implements MouseListener, MouseMotionLi
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -83,7 +93,7 @@ public class ScrollButton extends JPanel implements MouseListener, MouseMotionLi
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -114,8 +124,6 @@ public class ScrollButton extends JPanel implements MouseListener, MouseMotionLi
 
 	@Override
 	public void mouseWheelMoved(MouseWheelEvent e) {
-		// TODO Auto-generated method stub
-
+		parent.mouseWheelMoved(e);
 	}
-
 }
--- CodeEditorNew/src/view/ScrollPanel.java
+++ CodeEditorNew/src/view/ScrollPanel.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.Color;
+import java.awt.event.MouseWheelEvent;
 import java.util.ArrayList;
 import javax.swing.JPanel;
 
@@ -9,18 +10,36 @@ import utils.Theme;
 public class ScrollPanel extends JPanel {
 
 	private static Color BACKGROUND_COLOR = Theme.getBackgroundColor();
+	private ArrayList<ScrollButton> buttons;
+
+	private static int scrollAmount;
+	private static int SCROLL_FACTOR = 30;
 
 	public ScrollPanel(int x, int y, int width, int height, ArrayList<String> list) {
-		
+		buttons = new ArrayList<>();
 		setLayout(null);
 		setBackground(BACKGROUND_COLOR);
 		setBounds(x, y, width, height);
 
 		for (int i = 0; i < list.size(); i++) {
-			ScrollButton sb = new ScrollButton(list.get(i), width);
+			ScrollButton sb = new ScrollButton(list.get(i), this);
+			buttons.add(sb);
 			add(sb);
 		}
 
 		ScrollButton.resetCounter();
+	}
+
+	public ArrayList<ScrollButton> getButtons() {
+		return buttons;
+	}
+
+	public void mouseWheelMoved(MouseWheelEvent e) {
+		scrollAmount = -e.getWheelRotation() * SCROLL_FACTOR;
+		for (ScrollButton sb : buttons) {
+			sb.scroll(scrollAmount);
+		}
+		revalidate();
+		repaint();
 	}
 }
